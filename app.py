0445d713fcb089dce4603555c49baa0b7dc933f7
HongguLim <honggulim@MacBook-Pro-16.local>
2022-11-03 21:56:01 +0900

    commit

--- app.py
+++ app.py
@@ -1,58 +1,66 @@
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('mongodb+srv://test:sparta@cluster0.numzcdz.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/name", methods=["POST"])
-def name_post():
-    name_receive = request.form['name_give']
 
-    name_list = list(db.comment.find({}, {'_id':False}))
-    # count = len(name_list) + 1
+@app.route('/junho')
+def introduce_jun():
+    return render_template('junho.html')
 
-    doc = {
-        'name':name_receive
-    }
-    db.comment.insert_one(doc)
-    return jsonify({'msg': '등록 완료!'})
+@app.route('/soon')
+def introduce_soon():
+    return render_template('soon.html')
+
+
 
 
 @app.route("/comment", methods=["POST"])
 def comment_post():
     comment_receive = request.form['comment_give']
+    writer_receive = request.form['writer_give']
+    teamName_receive = request.form['teamName_give']
 
-    comment_list = list(db.comment.find({}, {'_id':False}))
-    # count = len(comment_list) + 1
+    comment_list = list(db.comment.find({}, {'_id': False}))
+    count = len(comment_list) + 1
 
     doc = {
-        'comment':comment_receive
+        'num': count,
+        'comment': comment_receive,
+        'del': 0,
+        'writer': writer_receive,
+        'name': 0,
+        'teamName': teamName_receive
     }
     db.comment.insert_one(doc)
-    # return jsonify({'msg': '등록 완료!'})
+
+    return jsonify({'msg': '댓글 완료!'})
 
 
+@app.route("/comment/del", methods=["POST"])
+def comment_del():
+    num_receive = request.form['num_give']
+
+    db.comment.update_one({'num': int(num_receive)}, {'$set': {'del': 1}})
+
+    return jsonify({'msg': '삭제 완료!'})
 
 
+@app.route("/comment", methods=["GET"])
+def comment_get():
+    comment_list = list(db.comment.find({}, {'_id': False}))
 
-
-#
-# @app.route("/name", methods=["GET"])
-# def name_get():
-#     name_list = list(db.name.find({},{'_id':False}))
-#     return jsonify({'names': name_list})
-#
-# @app.route("/comment", methods=["GET"])
-# def comment_get():
-#     comment_list = list(db.comment.find({},{'_id':False}))
-#     return jsonify({'comments': comment_list})
-
+    return jsonify({'comments': comment_list})
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5006, debug=True)
+    app.run('0.0.0.0', port=5020, debug=True)
---
+++ oldapp.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.numzcdz.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/name", methods=["POST"])
+def name_post():
+    name_receive = request.form['name_give']
+
+    name_list = list(db.comment.find({}, {'_id':False}))
+    count = len(name_list) + 1
+
+    doc = {
+        'name':name_receive
+    }
+    db.comment.insert_one(doc)
+    return jsonify({'msg': '등록 완료!'})
+
+
+@app.route("/comment", methods=["POST"])
+def comment_post():
+    comment_receive = request.form['comment_give']
+
+    comment_list = list(db.comment.find({}, {'_id':False}))
+    count = len(comment_list) + 1
+
+    doc = {
+        'comment':comment_receive
+    }
+    db.comment.insert_one(doc)
+    # return jsonify({'msg': '등록 완료!'})
+
+
+
+
+
+
+#
+# @app.route("/name", methods=["GET"])
+# def name_get():
+#     name_list = list(db.name.find({},{'_id':False}))
+#     return jsonify({'names': name_list})
+#
+# @app.route("/comment", methods=["GET"])
+# def comment_get():
+#     comment_list = list(db.comment.find({},{'_id':False}))
+#     return jsonify({'comments': comment_list})
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5026, debug=True)
